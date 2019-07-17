42ebf5d9746f720a5980e7ded7316b8f1a2a6c74
Ben Hamilton <ben.hamilton@me.com>
2019-07-17 12:58:15 +0100

    unit testing done for calculator test

--- js_calculator_start_point/tests/unit/calculator_spec.js
+++ js_calculator_start_point/tests/unit/calculator_spec.js
@@ -11,4 +11,58 @@ describe('calculator', function () {
     assert.equal(true, true)
   })
 
+  it('it can add 1 and 4', function(){
+    calculator.numberClick(1)
+    calculator.operatorClick('+')
+    calculator.numberClick(4)
+    calculator.operatorClick('=')
+    assert.strictEqual(calculator.runningTotal, 5)
+  })
+
+  it('it can subtract 4 from 7', function(){
+    calculator.numberClick(7)
+    calculator.operatorClick('-')
+    calculator.numberClick(4)
+    calculator.operatorClick('=')
+    assert.strictEqual(calculator.runningTotal, 3)
+  })
+
+  it('it can divide 21 by 7', function(){
+    calculator.numberClick(21)
+    calculator.operatorClick('/')
+    calculator.numberClick(7)
+    calculator.operatorClick('=')
+    assert.strictEqual(calculator.runningTotal, 3)
+  })
+
+  it('can concatenate multiple numbers clicked ', function(){
+    calculator.numberClick(3)
+    calculator.numberClick(4)
+    calculator.numberClick(9)
+    assert.strictEqual(calculator.runningTotal, 349)
+  })
+
+  it('can chain multiple operations together', function(){
+    calculator.numberClick(6)
+    calculator.operatorClick('*')
+    calculator.numberClick(6)
+    calculator.operatorClick('/')
+    calculator.numberClick(6)
+    calculator.operatorClick('=')
+    assert.strictEqual(calculator.runningTotal, 6)
+  })
+
+  it('can clear the running total without affecting the calculation', function(){
+    calculator.numberClick(2)
+    calculator.operatorClick('+')
+    calculator.numberClick(3)
+    calculator.operatorClick('+')
+    calculator.numberClick(6)
+    calculator.clearClick()
+    calculator.operatorClick('=')
+    assert.strictEqual(calculator.runningTotal, 5)
+  })
+
+
+
 });
